9335b2ecf41802e718e2a1d162a062ce3e8052ef
MiJin111 <wnalwls201@naver.com>
2021-07-16 15:19:45 +0900

    수정함

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -134,5 +134,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 
-
- 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIAUNASGDI4ZVX6LZFP'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'mijin1234'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
